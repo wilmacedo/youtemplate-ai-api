644a9428e396fa30b9471548681a8f599a58e4ba
Wil Macedo <wil.macedo.sa@gmail.com>
2023-09-12 20:17:54 -0300

    feat: :sparkles: add video transcription route

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { fastify } from "fastify";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 
@@ -6,6 +7,7 @@ const app = fastify();
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
 
 app
   .listen({
